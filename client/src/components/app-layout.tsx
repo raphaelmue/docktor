b677433cd723139c3a91bf1936704052cb4cd406
Raphael Müßeler <raphael@muesseler.de>
2026-02-22 18:10:46 +0100

    added test for data-table

--- client/src/components/app-layout.tsx
+++ client/src/components/app-layout.tsx
@@ -4,11 +4,13 @@ import {AppSidebar} from "@/components/app-sidebar";
 
 export function AppLayout() {
     return (
+        <div className={"font-mono"}>
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
                 <Outlet />
             </SidebarInset>
         </SidebarProvider>
+        </div>
     );
 }
